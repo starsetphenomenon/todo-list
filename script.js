30a4d3050e40bd9db0f24da040b6b8974bf50a7d
Roman <starsetphenomenon@gmail.com>
2022-06-18 12:33:42 +0300

    code optimization

--- script.js
+++ script.js
@@ -15,56 +15,50 @@ const addStorage = function () {
     localStorage.setItem('listCount', myList.childElementCount);
 };
 
-const getStorage = function () {
+//Get the story if it's not empty
+(function () {
     if (localStorage.getItem('listCount') >= 1) {
         myList.innerHTML = localStorage.getItem(`elem`);
     }
-};
+})();
 
-getStorage();
-
-Array.from(allDeleteBtn).forEach(elem => {
-    elem.addEventListener('click', e => {
+const onRemove = function (e) {
+    if (e.target.classList.contains('elemDelete')) {
         e.target.parentNode.classList.add('deleteAnim');
         setTimeout(function () {
             e.target.parentNode.remove();
             addStorage();
         }, 600);
-        addStorage();
-    });
-});
+    }
+};
+
+myList.addEventListener('click', onRemove);
+
+const onCheck = function (e) {
+    e.currentTarget.classList.toggle('checkedItem');
+    addStorage();
+};
+
 Array.from(myList.childNodes).forEach(elem => {
-    elem.addEventListener('click', e => {
-        elem.classList.toggle('checkedItem');
-        addStorage();
-    });
+    elem.addEventListener('click', onCheck);
 });
 
+// Dynamicly create list element
 const createListElem = function () {
     if (inputText.value.trim() !== '') {
         let listElement = document.createElement('li');
         let elementDelete = document.createElement('img');
         elementDelete.setAttribute('id', `elemDelete${listElements.length}`);
         elementDelete.setAttribute('class', 'elemDelete changeMonitor');
-        listElement.setAttribute('class', 'changeMonitor');
+        listElement.setAttribute('class', 'changeMonitor listElem');
         listElement.setAttribute('name', `elem${myList.childNodes.length}`);
         elementDelete.src = 'remove.svg';
         listElement.innerHTML = `<span class='elemText'>${inputText.value}</span>`;
         listElement.appendChild(elementDelete);
         myList.prepend(listElement);
         inputText.value = '';
-        elementDelete.addEventListener('click', e => {
-            e.target.parentNode.classList.add('deleteAnim');
-            setTimeout(function () {
-                e.target.parentNode.remove();
-                addStorage();
-            }, 600);
-            addStorage();
-        });
-        listElement.addEventListener('click', e => {
-            listElement.classList.toggle('checkedItem');
-            addStorage();
-        });
+        elementDelete.addEventListener('click', onRemove);
+        listElement.addEventListener('click', onCheck);
         // LOCAL STORAGE
         localStorage.setItem(`elem`, myList.innerHTML);
         localStorage.setItem('listCount', myList.childElementCount);
@@ -72,6 +66,7 @@ const createListElem = function () {
     }
 };
 
+//Delete all elements on button
 const deleteAll = function () {
     Array.from(myList.children).forEach(el => {
         el.classList.add('deleteAnim');
@@ -81,26 +76,22 @@ const deleteAll = function () {
         }, 600);
     });
 };
+
+//Check all elements on button
 const checkAll = function () {
     Array.from(myList.children).forEach(el => {
         el.classList.add('checkedItem');
     });
+    addStorage();
 };
 
-deleteItems.addEventListener('click', e => {
-    deleteAll();
-    addStorage();
-});
-checkItems.addEventListener('click', e => {
-    checkAll();
-    addStorage();
-});
-getTextBtn.addEventListener('click', createListElem);
-inputText.addEventListener('keyup', e => {
+const onEnterPress = function (e) {
     if (e.code === 'Enter' || e.code === 'NumpadEnter') {
         createListElem();
     }
-    if (keyboardEvent.code === 'Enter') {
-        e.target.blur();
-    }
-});
+};
+
+deleteItems.addEventListener('click', deleteAll);
+checkItems.addEventListener('click', checkAll);
+getTextBtn.addEventListener('click', createListElem);
+inputText.addEventListener('keyup', onEnterPress);
